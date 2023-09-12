9ef0dade6fe5423eee868773f70283e77577117e
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2023-09-13 01:06:10 +0530

    update web3

--- src/pages/Web3Example.js
+++ src/pages/Web3Example.js
@@ -3,6 +3,9 @@ import Web3 from "web3";
 
 function Web3Example() {
   const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
   const initWeb3 = async () => {
     try {
       if (window.ethereum) {
@@ -11,6 +14,8 @@ function Web3Example() {
         await window.ethereum.enable();
         setWeb3(web3Instance);
         console.log(web3Instance);
+        console.log(await web3Instance.eth.getAccounts());
+        console.log(web3Instance.eth.ens);
       } else {
         console.log("Please install MetaMask");
       }
@@ -18,11 +23,40 @@ function Web3Example() {
       console.log(err);
     }
   };
+  const fetchAccount = async () => {
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  };
 
+  const fetchBalance = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const balanceWei = await web3.eth.getBalance(accounts[0]);
+    const balanceEth = web3.utils.fromWei(balanceWei, "ether");
+    setBalance(balanceEth);
+  };
+  const handleSendTransaction = async () => {
+    if (web3) {
+      try {
+        await web3.eth.sendTransaction({
+          from: account,
+          to: "0xReceiverAddress", // Replace with actual recipient address
+          value: web3.utils.toWei("0.01", "ether"), // Sending 0.01 ETH
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
   return (
     <div>
       <h1>Web3.js</h1>
+      <h2>Ethereum Component</h2>
       <button onClick={initWeb3}>Initiate Web3</button>
+      {account && <p>Current Account: {account}</p>}{" "}
+      <button onClick={fetchAccount}>fetchAccount</button>
+      {balance && <p>Balance: {balance} ETH</p>}
+      <button onClick={fetchBalance}>fetchBalance</button>
+      <button onClick={handleSendTransaction}>Send Transaction</button>
     </div>
   );
 }
